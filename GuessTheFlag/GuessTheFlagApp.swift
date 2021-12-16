50ae6ef39bd200018d4058570038db796b02bf41
MarcinMatuk <marcin.mates@gmail.com>
2021-12-16 14:39:00 +0100

    dodanie calej apki

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Marcin Matuk on 09/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
